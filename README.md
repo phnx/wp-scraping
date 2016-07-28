341e477cd31acea8f1de135dbf76fd6e4fd3f0a0
wachiraphc <wcharoen@syr.edu>
2016-07-28 11:50:24 -0400

    remove file content

---
+++ README.md
@@ -0,0 +1,10 @@
+Wordpress Entry Scraping (July 2016)
+==============
+
+Basic requirement: an XML file exported from Wordpress, default settings
+--------------
+
+Unless it's working, I assume it's not - since you're reading this, review XML pathes in the code.
+Make sure they're consistent with your actual XML input.
+
+Enjoy mining.
---
+++ extract_entry.R
@@ -0,0 +1,174 @@
+# https://rstudio-pubs-static.s3.amazonaws.com/31867_8236987cf0a8444e962ccd2aec46d9c3.html
+
+Needed <- c("XML", "tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
+install.packages(Needed, dependencies=TRUE)   
+
+library(XML)
+library(RCurl)
+library(tm)
+library(ggplot2)
+library(wordcloud)
+library(SnowballC)
+library(RColorBrewer)
+library(topicmodels)
+
+
+setwd('Desktop/blog')
+mallet_stopwords1 <- c('a', 'able', 'about', 'above', 'according', 'accordingly', 'across', 'actually', 'after', 'afterwards', 'again', 'against', 'all', 'allow', 'allows', 'almost', 'alone', 'along', 'already', 'also', 'although', 'always', 'am', 'among', 'amongst', 'an', 'and', 'another', 'any', 'anybody', 'anyhow', 'anyone', 'anything', 'anyway', 'anyways', 'anywhere', 'apart', 'appear', 'appreciate', 'appropriate', 'are', 'around', 'as', 'aside', 'ask', 'asking', 'associated', 'at', 'available', 'away', 'awfully', 'b', 'be', 'became', 'because', 'become', 'becomes', 'becoming', 'been', 'before', 'beforehand', 'behind', 'being', 'believe', 'below', 'beside', 'besides', 'best', 'better', 'between', 'beyond', 'both', 'brief', 'but', 'by', 'c', 'came', 'can', 'cannot', 'cant', 'cause', 'causes', 'certain', 'certainly', 'changes', 'clearly', 'co', 'com', 'come', 'comes', 'concerning', 'consequently', 'consider', 'considering', 'contain', 'containing', 'contains', 'corresponding', 'could', 'course', 'currently', 'd', 'definitely', 'described', 'despite', 'did', 'different', 'do', 'does', 'doing', 'done', 'down', 'downwards', 'during', 'e', 'each', 'edu', 'eg', 'eight', 'either', 'else', 'elsewhere', 'enough', 'entirely', 'especially', 'et', 'etc', 'even', 'ever', 'every', 'everybody', 'everyone', 'everything', 'everywhere', 'ex', 'exactly', 'example', 'except', 'f', 'far', 'few', 'fifth', 'first', 'five', 'followed', 'following', 'follows', 'for', 'former', 'formerly', 'forth', 'four', 'from', 'further', 'furthermore', 'g', 'get', 'gets', 'getting', 'given', 'gives', 'go', 'goes', 'going', 'gone', 'got', 'gotten', 'greetings', 'h', 'had', 'happens', 'hardly', 'has', 'have', 'having', 'he', 'hello', 'help', 'hence', 'her', 'here', 'hereafter', 'hereby', 'herein', 'hereupon', 'hers', 'herself', 'hi', 'him', 'himself', 'his', 'hither', 'hopefully', 'how', 'howbeit', 'however', 'i', 'ie', 'if', 'ignored', 'immediate', 'in', 'inasmuch', 'inc', 'indeed', 'indicate', 'indicated', 'indicates', 'inner', 'insofar', 'instead', 'into', 'inward', 'is', 'it', 'its', 'itself', 'j', 'just', 'k', 'keep', 'keeps', 'kept', 'know', 'knows', 'known', 'l', 'last', 'lately', 'later', 'latter', 'latterly', 'least', 'less', 'lest', 'let', 'like', 'liked', 'likely', 'little', 'look', 'looking', 'looks', 'ltd', 'm', 'mainly', 'many', 'may', 'maybe', 'me', 'mean', 'meanwhile', 'merely', 'might', 'more', 'moreover', 'most', 'mostly', 'much', 'must', 'my', 'myself', 'n', 'name', 'namely', 'nd', 'near', 'nearly', 'necessary', 'need', 'needs', 'neither', 'never', 'nevertheless', 'new', 'next', 'nine', 'no', 'nobody', 'non', 'none', 'noone', 'nor', 'normally', 'not', 'nothing', 'novel', 'now', 'nowhere', 'o', 'obviously', 'of', 'off', 'often', 'oh', 'ok', 'okay', 'old', 'on', 'once', 'one', 'ones', 'only', 'onto', 'or', 'other', 'others', 'otherwise')
+mallet_stopwords2 <- c('ought', 'our', 'ours', 'ourselves', 'out', 'outside', 'over', 'overall', 'own', 'p', 'particular', 'particularly', 'per', 'perhaps', 'placed', 'please', 'plus', 'possible', 'presumably', 'probably', 'provides', 'q', 'que', 'quite', 'qv', 'r', 'rather', 'rd', 're', 'really', 'reasonably', 'regarding', 'regardless', 'regards', 'relatively', 'respectively', 'right', 's', 'said', 'same', 'saw', 'say', 'saying', 'says', 'second', 'secondly', 'see', 'seeing', 'seem', 'seemed', 'seeming', 'seems', 'seen', 'self', 'selves', 'sensible', 'sent', 'serious', 'seriously', 'seven', 'several', 'shall', 'she', 'should', 'since', 'six', 'so', 'some', 'somebody', 'somehow', 'someone', 'something', 'sometime', 'sometimes', 'somewhat', 'somewhere', 'soon', 'sorry', 'specified', 'specify', 'specifying', 'still', 'sub', 'such', 'sup', 'sure', 't', 'take', 'taken', 'tell', 'tends', 'th', 'than', 'thank', 'thanks', 'thanx', 'that', 'thats', 'the', 'their', 'theirs', 'them', 'themselves', 'then', 'thence', 'there', 'thereafter', 'thereby', 'therefore', 'therein', 'theres', 'thereupon', 'these', 'they', 'think', 'third', 'this', 'thorough', 'thoroughly', 'those', 'though', 'three', 'through', 'throughout', 'thru', 'thus', 'to', 'together', 'too', 'took', 'toward', 'towards', 'tried', 'tries', 'truly', 'try', 'trying', 'twice', 'two', 'u', 'un', 'under', 'unfortunately', 'unless', 'unlikely', 'until', 'unto', 'up', 'upon', 'us', 'use', 'used', 'useful', 'uses', 'using', 'usually', 'uucp', 'v', 'value', 'various', 'very', 'via', 'viz', 'vs', 'w', 'want', 'wants', 'was', 'way', 'we', 'welcome', 'well', 'went', 'were', 'what', 'whatever', 'when', 'whence', 'whenever', 'where', 'whereafter', 'whereas', 'whereby', 'wherein', 'whereupon', 'wherever', 'whether', 'which', 'while', 'whither', 'who', 'whoever', 'whole', 'whom', 'whose', 'why', 'will', 'willing', 'wish', 'with', 'within', 'without', 'wonder', 'would', 'would', 'x', 'y', 'yes', 'yet', 'you', 'your', 'yours', 'yourself', 'yourselves', 'z', 'zero')
+
+# Data Extraction
+xmlfile <- xmlParse('paintingdoodles.wordpress.2016-07-27.001.xml')
+
+nsTitle <- getNodeSet(xmlfile, '//channel/item/title')
+nsContent <- getNodeSet(xmlfile, '//channel/item/content:encoded')
+
+counter <- 1
+for(node in nsContent) {
+  title <- getChildrenStrings(nsTitle[[counter]])
+  title <- gsub("/", "-", title, perl=TRUE)
+  
+  content <- getChildrenStrings(node)
+  content <- gsub("<.*>", "", content, perl=TRUE)
+  
+  if(content != "") {
+    #print(content)
+    write(content, file = paste("export/",title,".txt", sep = "")) 
+  }
+  counter <- counter + 1
+}
+
+# Re-Import data
+cname <- file.path("export")   
+docs <- Corpus(DirSource(cname))
+docs <- tm_map(docs, removePunctuation) 
+docs <- tm_map(docs, removeNumbers)   
+docs <- tm_map(docs, tolower)   
+
+docs <- tm_map(docs, removeWords, stopwords("english"))   
+docs <- tm_map(docs, removeWords, mallet_stopwords1)   
+docs <- tm_map(docs, removeWords, mallet_stopwords2)
+docs <- tm_map(docs, stemDocument)  
+
+docs <- tm_map(docs, stripWhitespace)  
+docs <- tm_map(docs, PlainTextDocument)  
+
+dtm <- DocumentTermMatrix(docs)   
+
+freq <- colSums(as.matrix(dtm))   
+
+ord <- order(freq)   
+freq[tail(ord)]  
+freq[ord[1:100]]
+
+head(table(freq), 20)   
+findFreqTerms(dtm, lowfreq=30) 
+
+
+wf <- data.frame(word=names(freq), freq=freq)   
+head(wf)
+
+# plot frequency
+p <- ggplot(subset(wf, freq>30), aes(word, freq))    
+p <- p + geom_bar(stat="identity")   
+p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))   
+p   
+
+# wordcloud
+set.seed(142)   
+wordcloud(names(freq), freq, min.freq=25)   
+
+# ================================================================
+
+# Topic Model https://eight2late.wordpress.com/2015/09/29/a-gentle-introduction-to-topic-modeling-using-r/
+setwd('export')
+filenames <- list.files(getwd(),pattern="*.txt")
+files <- lapply(filenames,readLines)
+docs <- Corpus(VectorSource(files))
+writeLines(as.character(docs[[30]]))
+docs <-tm_map(docs,content_transformer(tolower))
+
+toSpace <- content_transformer(function(x, pattern) { return (gsub(pattern, " ", x))})
+docs <- tm_map(docs, toSpace, "-")
+docs <- tm_map(docs, toSpace, "’")
+docs <- tm_map(docs, toSpace, "‘")
+docs <- tm_map(docs, toSpace, "•")
+docs <- tm_map(docs, toSpace, '"')
+
+#remove punctuation
+docs <- tm_map(docs, removePunctuation)
+#Strip digits
+docs <- tm_map(docs, removeNumbers)
+#remove stopwords
+docs <- tm_map(docs, removeWords, stopwords("english"))
+#define and eliminate all custom stopwords
+myStopwords <- c("can", "say","one","way","use",
+                 "also","howev","tell","will",
+                 "much","need","take","tend","even",
+                 "like","particular","rather","said",
+                 "get","well","make","ask","come","end",
+                 "first","two","help","often","may",
+                 "might","see","someth","thing","point",
+                 "post","look","right","now","think","‘ve ",
+                 "‘re ","anoth","put","set","new","good",
+                 "want","sure","kind","larg","yes,","day","etc",
+                 "quit","sinc","attempt","lack","seen","awar",
+                 "littl","ever","moreov","though","found","abl",
+                 "enough","far","earli","away","achiev","draw",
+                 "last","never","brief","bit","entir","brief",
+                 "great","lot")
+docs <- tm_map(docs, removeWords, myStopwords)
+docs <- tm_map(docs, removeWords, mallet_stopwords1)   
+docs <- tm_map(docs, removeWords, mallet_stopwords2)
+
+#remove whitespace
+docs <- tm_map(docs, stripWhitespace)
+#Good practice to check every now and then
+writeLines(as.character(docs[[30]]))
+#Stem document
+docs <- tm_map(docs,stemDocument)
+
+#inspect a document as a check
+writeLines(as.character(docs[[30]]))
+
+
+#Create document-term matrix
+dtm <- DocumentTermMatrix(docs)
+#convert rownames to filenames
+rownames(dtm) <- filenames
+#collapse matrix by summing over columns
+freq <- colSums(as.matrix(dtm))
+#length should be total number of terms
+length(freq)
+#create sort order (descending)
+ord <- order(freq,decreasing=TRUE)
+#List all terms in decreasing order of freq and write to disk
+freq[ord]
+write.csv(freq[ord],"word_freq.csv")
+
+#Set parameters for Gibbs sampling
+burnin <- 4000
+iter <- 2000
+thin <- 500
+seed <-list(2003,5,63,100001,765)
+nstart <- 5
+best <- TRUE
+
+#Number of topics
+k <- 10
+#Run LDA using Gibbs sampling
+ldaOut <-LDA(dtm,k, method="Gibbs", control=list(nstart=nstart, seed = seed, best=best, burnin = burnin, iter = iter, thin=thin))
+
+#write out results
+#docs to topics
+ldaOut.topics <- as.matrix(topics(ldaOut))
+write.csv(ldaOut.topics,file=paste("LDAGibbs",k,"DocsToTopics.csv"))
+
+#top 6 terms in each topic
+ldaOut.terms <- as.matrix(terms(ldaOut,6))
+write.csv(ldaOut.terms,file=paste("LDAGibbs",k,"TopicsToTerms.csv"))
+
+#probabilities associated with each topic assignment
+topicProbabilities <- as.data.frame(ldaOut@gamma)
+write.csv(topicProbabilities,file=paste("LDAGibbs",k,"TopicProbabilities.csv"))
+
